a8601b6377f3b032b8cb0a2a69458bb5e7a3bfc6
Pulkit Choudhary <104904183+ozesince02@users.noreply.github.com>
2023-12-06 17:06:20 +0530

    Update capm_functions.py

--- capm_functions.py
+++ capm_functions.py
@@ -1,5 +1,9 @@
 import plotly.express as px
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import numpy as np
+import streamlit as st
 
 
 def interactive_plot(df):
@@ -47,3 +51,27 @@ def calculate_beta(stocks_daily_return, stock, index_name):
 
     b, a = np.polyfit(stocks_daily_return[index_name], stocks_daily_return[stock], 1)
     return b, a
+
+
+def calculate_amount(p, t, r):
+    amount = p*(1+(r/100))**t
+    return round(amount, 2)
+
+
+def plot_investment_period(df, stock_selected, time):
+    last_day_df = df.iloc[[-1]]
+    last_day_df.reset_index(inplace=True)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    investment_period = df[df['Date'] >= last_day_df['Date'].values[0] - pd.DateOffset(years=time)]
+
+    sns.lineplot(x='Date', y=stock_selected, data=df, label='Historical Prices')
+    sns.lineplot(x='Date', y=stock_selected, data=investment_period, label=f'Investment Period ({time} years)', color='orange')
+
+    plt.title(f'{stock_selected} Stock Performance Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price')
+    plt.legend()
+
+    # Use st.pyplot to display the Matplotlib plot
+    st.pyplot(fig)
